6c67224b44d8923864283f665eb085120942fb8b
James Gould <nobleburgundy@gmail.com>
2020-12-26 17:07:34 -0600

    Cleaned up a bunch of commented code. Filled out some more employee functions

--- server.js
+++ server.js
@@ -1,63 +1,9 @@
 const DB = require("./lib/DB");
 const inquirer = require("inquirer");
-
 const db = new DB();
-// db.getAllEmployees();
-// db.getAllEmployeesByDepartment("Engineering");
-// db.addRole("QA Engineer II", "120000", 1);
-// db.getAllRoles();
 
-// db2.addRole("QA Technician X", "125000", 3);
-// db2.getAllEmployees();
-// db2.getAllRoles();
-
-// let departments = db.getAllDepartments(function (res) {
-//   res.forEach((element) => {
-//     console.log(element.department_name);
-//   });
-// });
-
-// let depArr = [];
-// db.getAllEmployees((cb) => {
-//   cb.forEach((d) => {
-//     console.log(d.department_name);
-//     depArr.push(d.department_name);
-//   });
-//   console.log(depArr);
-// });
-
-// const genList = (list) => {
-//   const choices = list.map((item, index) => {
-//     return {
-//       key: index,
-//       name: `${item.id}`,
-//     };
-//   });
-//   return {
-//     type: "rawlist",
-//     message: "Which order to pick",
-//     name: "orders",
-//     choices: choices,
-//   };
-// };
-
-// let roles = db.getAllRoles(function (res) {
-//   res.forEach((element) => {
-//     console.log(element.title);
-//   });
-// });
-
-// db.getEmployeeByDepartment("Sales", (res) => {
-//   console.table(res);
-// });
-
-// db.endConnecton();
-// db.getAllDepartments();
-// console.log(roles);
-
-// db.addEmployee("James", "Gould", 3, 6);
-// db.getAllEmployees();
 start();
+
 function start() {
   inquirer
     .prompt({
@@ -66,8 +12,10 @@ function start() {
       type: "list",
       choices: [
         "View All Employees",
-        "View All Employees By Department",
-        "View All Employees By Role",
+        "View Employees By Department",
+        "View Employees By Role",
+        "View Departments",
+        "View Roles",
         "Add Employee",
         "Add Role",
         "Add Department",
@@ -79,17 +27,47 @@ function start() {
         case "View All Employees":
           db.getAllEmployees((results) => {
             console.table(results);
+            db.endConnecton();
+            start();
           });
+
           break;
-        case "View All Employees By Department":
+        case "View Employees By Department":
           viewByDepartment();
           break;
-        case "View All Employees By Role":
+        case "View Employees By Role":
           viewByRole();
+          break;
+        case "View Roles":
+          db.getAllRoles((res) => {
+            console.table(res);
+          });
+          break;
+        case "View Departments":
+          db.getAllDepartments((res) => {
+            console.table(res);
+          });
           break;
         case "Add Employee":
           addEmployee();
+          break;
+        case "Add Department":
+          inquirer
+            .prompt({
+              name: "department",
+              message: "Department name:",
+              type: "Input",
+            })
+            .then((answer) => {
+              db.addDepartment(answer.department);
+            });
+          break;
+        case "Add Role":
+          addRole();
+          break;
         default:
+          // Exit was choosen
+          db.endConnecton();
           break;
       }
     });
@@ -105,6 +83,7 @@ viewByDepartment = () => {
     .then((answer) => {
       db.getEmployeeByDepartment(answer.department, (res) => {
         console.table(res);
+        start();
       });
     })
     .finally(() => {
@@ -122,6 +101,7 @@ viewByRole = () => {
     .then((answer) => {
       db.getEmployeesByRole(answer.role, (res) => {
         console.table(res);
+        start();
       });
     })
     .finally(() => {
@@ -155,6 +135,35 @@ addEmployee = () => {
     ])
     .then((answer) => {
       db.addEmployee(answer.firstName, answer.lastName, answer.roleId, answer.managerId);
+      start();
+    })
+    .finally(() => {
+      db.endConnecton();
+    });
+};
+
+addRole = () => {
+  inquirer
+    .prompt([
+      {
+        name: "title",
+        message: "Role Title:",
+        type: "input",
+      },
+      {
+        name: "salary",
+        message: "Role Salary:",
+        type: "input",
+      },
+      {
+        name: "departmentId",
+        message: "Department Id: ",
+        type: "input",
+      },
+    ])
+    .then((answer) => {
+      db.addRole(answer.title, answer.salary, answer.departmentId);
+      start();
     })
     .finally(() => {
       db.endConnecton();
