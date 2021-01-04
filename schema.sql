677e423c50a029e4c6258596eaf35806530134a2
James Gould <nobleburgundy@gmail.com>
2021-01-03 18:52:16 -0700

    Added 'NOT NULL' to some fields in schema

--- schema.sql
+++ schema.sql
@@ -10,24 +10,24 @@ USE employee_management_db;
 
 CREATE TABLE department (
 	id INT AUTO_INCREMENT,
-	department_name VARCHAR(30),
+	department_name VARCHAR(30) NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE role_table (
 	id INT AUTO_INCREMENT,
-	title VARCHAR(30),
-    salary VARCHAR(30),
-    department_id int,
+	title VARCHAR(30) NOT NULL,
+    salary VARCHAR(30) NOT NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES department(id),
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE employee (
 	id INT AUTO_INCREMENT,
-	first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
+	first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
     manager_id INT,
     FOREIGN KEY (manager_id) REFERENCES employee (id), 
     FOREIGN KEY (role_id) REFERENCES role_table (id),
